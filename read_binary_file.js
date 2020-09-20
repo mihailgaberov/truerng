9e1ad5fb2d5076f69f8468f099ddb2e806d804e8
mihailgaberov <mihail.gaberov@gmail.com>
2020-09-20 11:01:45 +0300

    :alembic: Get only 75 unique numbers.

--- read_binary_file.js
+++ read_binary_file.js
@@ -3,9 +3,12 @@ const fs = require('fs')
 fs.readFile('./random.bin', (err, data) => {
     if (err) throw err;
 
-    const numbers = JSON.parse(JSON.stringify(data)).data;
+    const allNumbersInRange75 = Array
+        .from(JSON.parse(JSON.stringify(data)).data)
+        .filter(num => num <= 75 && num > 0);
+    const only75UniqueNumbers = new Set(allNumbersInRange75);
 
-    fs.writeFile('random-numbers.txt', numbers, function(err) {
+    fs.writeFile('random-numbers.txt', Array.commfrom(only75UniqueNumbers), function(err) {
         if (err) {
             return console.log(err);
         }
