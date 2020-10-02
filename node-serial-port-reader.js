7156d9f84a8a24aac5e334777657375c8d881ce6
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-10-02 08:07:59 +0300

    :alembic: Adding nodejs serial port reader.

--- node-serial-port-reader.js
+++ node-serial-port-reader.js
@@ -1,14 +1,38 @@
 const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+
 
 SerialPort.list().then(data => console.log('Available ports: ', data))
 
 const serialPort = new SerialPort('COM6', { autoOpen: false })
 
-serialPort.open(() => {
+serialPort.open((err) => {
+    if (err) {
+        return console.log('Error opening port: ', err.message)
+    }
+
     console.log(`Port opended.`)
 
-
-    console.log('Baud rate: ', serialPort.baudRate)
+    console.log('Baud rate before update: ', serialPort.baudRate)
     serialPort.update({ baudRate: 300 })
-    console.log('Baud rate: ', serialPort.baudRate)
+
+    console.log('Baud rate after update: ', serialPort.baudRate)
+    console.log('Binding object: ', serialPort.binding)
+    console.log('Is open: ', serialPort.isOpen)
+    console.log('Path: ', serialPort.path)
 })
+
+
+// Read data that is available but keep the stream in "paused mode"
+/* serialPort.on('readable', function () {
+    console.log('Data:', serialPort.read())
+})
+ */
+// Switches the port into "flowing mode"
+serialPort.on('data', function (data) {
+    console.log('Data:', data)
+})
+
+// Pipe the data into another stream (like a parser or standard out)
+const lineStream = serialPort.pipe(new Readline())
+lineStream.on('data', console.log)
