b4f84030af0cb5a1fa06b512d1e43989bfac2a8c
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-10-06 08:13:35 +0300

    :sparkles: Finished serial port reader.

--- node-serial-port-reader.js
+++ node-serial-port-reader.js
@@ -1,39 +1,46 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
 
-// SerialPort.list().then(data => console.log('Available ports: ', data))
+let path = '';
+SerialPort.list().then(data => {
+    // Get the path of the port on which TrueRNG is attached
+    data.forEach(port => {
+        if (port && port.manufacturer && port.manufacturer.includes('ubld.it')) {
+            path = port.path;
+            console.log('Port path found: ', path);
+        } else {
+            return console.log('Cannot find TrueRNG in the available ports.');
+        }
+    });
 
-const serialPort = new SerialPort('COM6', { autoOpen: false })
+    const serialPort = new SerialPort(path, { autoOpen: false });
 
-serialPort.open((err) => {
-    if (err) {
-        return console.log('Error opening port: ', err.message)
-    }
+    serialPort.open((err) => {
+        if (err) {
+            return console.log('Error opening port: ', err.message);
+        }
 
-    console.log(`Port opended.`)
+        console.log(`Port opended.`);
 
-    console.log('Baud rate before update: ', serialPort.baudRate)
-    serialPort.update({ baudRate: 300 })
-
-    console.log('Baud rate after update: ', serialPort.baudRate)
-    // console.log('Binding object: ', serialPort.binding)
-    // console.log('************ Setting encoding to UTF-8')
-})
+        console.log('Baud rate before update: ', serialPort.baudRate);
+        serialPort.update({ baudRate: 300 });
+        console.log('Baud rate after update: ', serialPort.baudRate);
+    })
 
 
-// Read data that is available but keep the stream in "paused mode"
-serialPort.on('readable', function () {
-    serialPort.read(512)
-})
+    // Read data that is available but keep the stream in "paused mode"
+    serialPort.on('readable', function () {
+        serialPort.read(512);
+    })
+
+    let arrNumbers = [];
 
 // Switches the port into "flowing mode"
-serialPort.on('data', function (data) {
-    console.log('Data:', JSON.stringify(data))
+serialPort.on('data', function (receivedData) {
+    if (arrNumbers.length === 0) {
+        // console.log('================== Data:', JSON.stringify(receivedData))
+        arrNumbers = JSON.parse(JSON.stringify(receivedData)).data;
+        console.log('>>>> nums: ', arrNumbers);
+    }
 })
-
-// Pipe the data into another stream (like a parser or standard out)
-// const parser = serialPort.pipe(new Readline())
-
-// parser.on('data', (data) => {
-//     console.log(JSON.stringify(data))
-// })
+});
